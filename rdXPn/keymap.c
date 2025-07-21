2ce5e5854800315c83ea9dd23ca43a8b3cc7afb8
Tim Wolff <timtwotoes@me.com>
2025-07-21 22:27:53 +0200

    Remove null terminated array for key overrides

--- rdXPn/keymap.c
+++ rdXPn/keymap.c
@@ -212,8 +212,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, LT(1,KC_BSPC), KC_DELETE);
 
 const key_override_t **key_overrides = (const key_override_t *[]) {
-  &delete_key_override,
-  NULL
+  &delete_key_override
 };
 
 
